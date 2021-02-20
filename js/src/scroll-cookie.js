901962800eccd2025c0f18d81a1b2c6c316adc8b
天火流光 <gjt6636@vip.qq.com>
2021-02-20 15:55:09 +0800

    Site updated: 2021-02-20 15:54:49

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Feb 19 2021 15:08:25 GMT+0800 (GMT+08:00)
+// build time:Sat Feb 20 2021 15:54:08 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var rpath=window.location.href.replace(window.location.origin,"");var timeout;$(window).on("scroll",function(){clearTimeout(timeout);timeout=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+rpath,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var cvalues=Cookies.get("scroll-cookie").split("|");if(cvalues[1]==rpath){$(window).scrollTop(cvalues[0])}}});
 //rebuild by neat 
