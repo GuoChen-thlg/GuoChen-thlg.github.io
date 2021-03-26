8ee6e2476a2e577264e2cbe8d7dab7fb85226ba6
天火流光 <gc.thlg@gmail.com>
2021-03-26 13:48:11 +0800

    Site updated: 2021-03-26 13:47:44

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 25 2021 09:32:00 GMT+0800 (GMT+08:00)
+// build time:Fri Mar 26 2021 13:46:12 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var rpath=window.location.href.replace(window.location.origin,"");var timeout;$(window).on("scroll",function(){clearTimeout(timeout);timeout=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+rpath,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var cvalues=Cookies.get("scroll-cookie").split("|");if(cvalues[1]==rpath){$(window).scrollTop(cvalues[0])}}});
 //rebuild by neat 
